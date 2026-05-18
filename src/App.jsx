3bb5d5d025e9c445f1db78984e1211e549c7606e
federicosimone <federico.simone15@gmail.com>
2026-05-18 11:33:54 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+//import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import './App.css'
+
+function App() {
+  //const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" />
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
