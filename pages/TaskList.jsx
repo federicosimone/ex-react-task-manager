0bc26fcc6521d4f0b5527717ef861633f254b76a
federicosimone <federico.simone15@gmail.com>
2026-05-21 15:41:53 +0200

    input di ricerca

--- pages/TaskList.jsx
+++ pages/TaskList.jsx
@@ -1,6 +1,6 @@
 import { useTaskContext } from "../src/context/TaskContext";
 import TaskRow from "../components/TaskRow";
-import { useState, useMemo } from "react";
+import { useState, useMemo, useCallback, useEffect } from "react";
 
 function TaskList() {
 
@@ -9,10 +9,20 @@ function TaskList() {
     const [sortBy, setSortBy] = useState("createdAt")
     const [sortOrder, setSortOrder] = useState(1)
 
+    const [inputValue, setInputValue] = useState("");
+    const [searchQuery, setSearchQuery] = useState("")
+    console.log(searchQuery)
+
     const sortedTasks = useMemo(() => {
         const tasks2 = [...tasks]
 
-        return tasks2.sort((a, b) => {
+        const taskFiltered = tasks2.filter(task => {
+            return task.title.toLowerCase().includes(searchQuery.toLowerCase())
+        })
+
+
+
+        return taskFiltered.sort((a, b) => {
             let result = 0;
             if (sortBy === "title") {
                 result = a.title.localeCompare(b.title)
@@ -26,9 +36,9 @@ function TaskList() {
                 result = new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
             }
 
-            return result * sortOrder;
+            return result * sortOrder;   // moltiplico result per lo stato sortOrder che ho dichiarato 1, in modo da far si  che sortOrder task cambi il segno
         });
-    }, [tasks, sortBy, sortOrder]);
+    }, [tasks, sortBy, sortOrder, searchQuery]);
 
     const handleSort = (field) => {
         if (sortBy === field) {
@@ -39,29 +49,62 @@ function TaskList() {
         }
     };
 
+    function debounce(callback, delay) {
+        let timer
+        return (value) => {
+            clearTimeout(timer);
+            timer = setTimeout(() => {
+                callback(value);
+            }, delay);
+        };
+    };
+
+
+
+
+
+    const setQueryDebounced = useCallback(
+        debounce(setSearchQuery, 1000)
+        , []);
+
+
+
+
 
     return (
-        <div className="mt-3">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col"><button className="btn" onClick={() => handleSort("title")}>Titolo</button></th>
-                        <th scope="col"><button className="btn" onClick={() => handleSort("status")}>Stato</button></th>
-                        <th scope="col"><button className="btn" onClick={() => handleSort("createdAt")}>Data di Creazione</button></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {sortedTasks.map(task => {
-                        return <TaskRow key={task.id}
-                            id={task.id}
-                            title={task.title}
-                            status={task.status}
-                            createdAt={task.createdAt} />
-                    })}
+        <>
 
-                </tbody>
-            </table>
-        </div>
+            <div className="mt-3">
+                <div className="mb-3">
+                    <label className=" form-label">Cerca task</label>
+                    <input className="form-control" type="text" name="" value={inputValue} onChange={(e) => {
+                        const value = e.target.value;
+                        setInputValue(value)
+                        setQueryDebounced(value)
+                    }}></input>
+                </div>
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col"><button className="btn" onClick={() => handleSort("title")}>Titolo</button></th>
+                            <th scope="col"><button className="btn" onClick={() => handleSort("status")}>Stato</button></th>
+                            <th scope="col"><button className="btn" onClick={() => handleSort("createdAt")}>Data di Creazione</button></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {sortedTasks.map(task => {
+                            return <TaskRow key={task.id}
+                                id={task.id}
+                                title={task.title}
+                                status={task.status}
+                                createdAt={task.createdAt} />
+                        })}
+
+                    </tbody>
+                </table>
+            </div>
+        </>
+
     );
 }
 
